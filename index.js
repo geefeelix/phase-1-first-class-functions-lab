fbe69c51b394a0605c307c4f0700f29dc0f4c025
godspower <geefeelix@gmail.com>
2022-04-29 20:03:28 -0400

    commit tree fc808180ed27b8ea02783027dc4405117c419276

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers) {
+    return arrayOfDrivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(arrayOfDrivers) {
+    return arrayOfDrivers.slice(2,4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fareMultiplier) {
+
+}
